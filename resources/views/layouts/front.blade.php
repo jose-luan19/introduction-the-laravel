86d4a661c26b42165bff9b921b929feadbe56bbe
José Luan Lima da Costa <joseluan620@gmail.com>
2022-03-06 10:46:22 -0300

    botões de login e register na home

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -7,14 +7,75 @@
     <title>Marketplace L6</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
     <style>
         .front.row {
             margin-bottom: 40px;
         }
+        html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+            .full-height {
+                height: 04vh;
+            }
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+            .position-ref {
+                position: relative;
+            }
+            .top-right {
+                position: absolute;
+                right: 20px;
+                top: 4px;
+            }
+            .content {
+                text-align: center;
+            }
+            .title {
+                font-size: 84px;
+            }
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+            .m-b-md {
+                margin-bottom: 30px;
+            }
     </style>
     @yield('stylesheets')
 </head>
 <body>
+    @guest
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+            @auth
+            @else
+                <a href="{{ route('login') }}">Login</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
+                </div>
+            @endif
+        </div>
+    @endguest
+
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin-bottom: 40px;">
 
         <a class="navbar-brand" href="{{route('home')}}">Marketplace L6</a>
@@ -42,6 +103,20 @@
                         <a class="nav-link" href="{{route('admin.categories.index')}}">Categorias</a>
                     </li>
                 </ul>
+
+                <div class=" my-2 my-lg-0">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <span class="nav-link">{{auth()->user()->name}}</span>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" onclick="event.preventDefault(); document.querySelector('form.logout').submit();">Sair</a>
+                            <form action="{{route('logout')}}" class="logout" method="POST" style="display:none;">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </div>
             @endauth
             <div class="my-2 my-lg-0">
                 <ul class="navbar-nav mr-auto">
